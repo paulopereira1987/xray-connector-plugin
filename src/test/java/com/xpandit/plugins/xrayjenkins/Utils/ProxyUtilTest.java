9849cda15e0a8d2548dd5c8e65bddc6af9c5486e
Fábio Cunha <fabio.cunha@xpand-it.com>
2020-10-13 15:40:46 +0100

    Added checks for alternative no proxy hosts format

--- src/test/java/com/xpandit/plugins/xrayjenkins/Utils/ProxyUtilTest.java
+++ src/test/java/com/xpandit/plugins/xrayjenkins/Utils/ProxyUtilTest.java
@@ -28,9 +28,12 @@ public class ProxyUtilTest {
     private static final String PROXY_DOMAIN = "proxydomain.com";
     private static final int PROXY_PORT = 8080;
     private static final String JIRA_DOMAIN = "jiradomain.com";
+    private static final String JIRA_ALTERNATIVE_DOMAIN = "www.my-jira-server.com/";
     private static final int JIRA_PORT = 8080;
     private static final String JIRA_HTTP_URL = "http://" + JIRA_DOMAIN;
+    private static final String JIRA_ALTERNATIVE_HTTP_URL = "http://" + JIRA_ALTERNATIVE_DOMAIN;
     private static final String JIRA_HTTP_URL_WITH_PORT = "http://" + JIRA_DOMAIN + ":" + JIRA_PORT;
+    private static final String JIRA_ALTERNATIVE_HTTP_URL_WITH_PORT = "http://" + JIRA_ALTERNATIVE_DOMAIN + ":" + JIRA_PORT;
 
     @Mock
     private Jenkins jenkins;
@@ -72,26 +75,34 @@ public class ProxyUtilTest {
         createProxyBeanAndXrayClient();
         assertTrue(proxyBean.useProxy(JIRA_HTTP_URL));
         assertTrue(proxyBean.useProxy(JIRA_HTTP_URL_WITH_PORT));
+        assertTrue(proxyBean.useProxy(JIRA_ALTERNATIVE_HTTP_URL));
+        assertTrue(proxyBean.useProxy(JIRA_ALTERNATIVE_HTTP_URL_WITH_PORT));
     }
 
     @Test
     public void testCreateProxyWithProxyAndNoProxyHost() {
         setProxyConfiguration(PROXY_DOMAIN, PROXY_PORT);
-        setNoProxyHost(JIRA_DOMAIN);
+        setNoProxyHost(JIRA_DOMAIN + "\n" +
+                       JIRA_ALTERNATIVE_DOMAIN);
 
         createProxyBeanAndXrayClient();
         assertFalse(proxyBean.useProxy(JIRA_HTTP_URL));
         assertFalse(proxyBean.useProxy(JIRA_HTTP_URL_WITH_PORT));
+        assertFalse(proxyBean.useProxy(JIRA_ALTERNATIVE_HTTP_URL));
+        assertFalse(proxyBean.useProxy(JIRA_ALTERNATIVE_HTTP_URL_WITH_PORT));
     }
 
     @Test
     public void testCreateProxyWithProxyAndNoProxyHostAndPort() {
         setProxyConfiguration(PROXY_DOMAIN, PROXY_PORT);
-        setNoProxyHost(JIRA_DOMAIN + ":" + JIRA_PORT);
+        setNoProxyHost(JIRA_DOMAIN + ":" + JIRA_PORT + "\n" +
+                       JIRA_ALTERNATIVE_DOMAIN + ":" + JIRA_PORT);
 
         createProxyBeanAndXrayClient();
         assertTrue(proxyBean.useProxy(JIRA_HTTP_URL));
         assertFalse(proxyBean.useProxy(JIRA_HTTP_URL_WITH_PORT));
+        assertTrue(proxyBean.useProxy(JIRA_ALTERNATIVE_HTTP_URL));
+        assertFalse(proxyBean.useProxy(JIRA_ALTERNATIVE_HTTP_URL_WITH_PORT));
     }
 
     @Test
@@ -100,11 +111,14 @@ public class ProxyUtilTest {
         setNoProxyHost("host1.com \n" +
                        "host2.com \n" +
                        "host3.com \n" +
-                       JIRA_DOMAIN);
+                       JIRA_DOMAIN + "\n" +
+                       JIRA_ALTERNATIVE_DOMAIN);
 
         createProxyBeanAndXrayClient();
         assertFalse(proxyBean.useProxy(JIRA_HTTP_URL));
         assertFalse(proxyBean.useProxy(JIRA_HTTP_URL_WITH_PORT));
+        assertFalse(proxyBean.useProxy(JIRA_ALTERNATIVE_HTTP_URL));
+        assertFalse(proxyBean.useProxy(JIRA_ALTERNATIVE_HTTP_URL_WITH_PORT));
     }
 
     @Test
@@ -115,6 +129,8 @@ public class ProxyUtilTest {
         createProxyBeanAndXrayClient();
         assertTrue(proxyBean.useProxy(JIRA_HTTP_URL));
         assertTrue(proxyBean.useProxy(JIRA_HTTP_URL_WITH_PORT));
+        assertTrue(proxyBean.useProxy(JIRA_ALTERNATIVE_HTTP_URL));
+        assertTrue(proxyBean.useProxy(JIRA_ALTERNATIVE_HTTP_URL_WITH_PORT));
     }
 
     @Test
@@ -125,6 +141,8 @@ public class ProxyUtilTest {
         createProxyBeanAndXrayClient();
         assertTrue(proxyBean.useProxy(JIRA_HTTP_URL));
         assertTrue(proxyBean.useProxy(JIRA_HTTP_URL_WITH_PORT));
+        assertTrue(proxyBean.useProxy(JIRA_ALTERNATIVE_HTTP_URL));
+        assertTrue(proxyBean.useProxy(JIRA_ALTERNATIVE_HTTP_URL_WITH_PORT));
     }
 
 }
